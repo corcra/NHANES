23792735ef898a1c53f45cae1f0554442ffe8f9c
Stephanie Hyland <steph.hyland@gmail.com>
2014-01-16 11:14:31 -0500

    plotting?

--- do_ggm.r
+++ do_ggm.r
@@ -1,5 +1,5 @@
 library(ggm)
-THRESH<-0.051
+THRESH<-0.051*5
 
 data<-read.table("lab/lab_data_high_qual.txt",header=T,row.names=1)
 # tidy up
@@ -23,6 +23,23 @@ rownames(edge_mat)<-colnames(data)
 colnames(edge_mat)<-colnames(data)
 amat<-adjMatrix(edge_mat)
 cg<-fitConGraph(amat, S, n)
+#covg<-fitCovGraph(amat, S, n)
 
 congraph<-(cg$Shat>THRESH)*1
+#covgraph<-(covg$Shat>THRESH)*1
+ticks<-seq(nrow(edge_mat))/nrow(edge_mat)
+labels<-rownames(edge_mat)
 
+pdf('ggm_network.pdf')
+par(las=2,cex=0.5,mar=c(5, 4, 4, 2) + 3.5)
+image(congraph,col=c("white","black"),axes=FALSE)
+axis(1,labels=labels,at=ticks)
+axis(2,labels=labels,at=ticks)
+dev.off()
+
+#pdf('covgraph_network.pdf')
+#par(las=2,cex=0.5,mar=c(5, 4, 4, 2) + 3.5)
+#image(covgraph,col=c("white","black"),axes=FALSE)
+#axis(1,labels=labels,at=ticks)
+#axis(2,labels=labels,at=ticks)
+#dev.off()
